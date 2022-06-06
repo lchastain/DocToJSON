58c4e1c47bf4567c06f77a3251ef3ad19aa9df5d
Lee Chastain <lee@doughmain.net>
2022-06-06 10:03:30 +0400

    Initial commit.

--- src/main/java/StyledDocumentResearch.java
+++ src/main/java/StyledDocumentResearch.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import javax.swing.*;
 import javax.swing.text.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
